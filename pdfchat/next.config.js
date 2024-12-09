74cc3e876dde6a42a397b12000c8baa211396b38
Muskan Pandey <113443689+pandey03muskan@users.noreply.github.com>
2024-12-09 16:11:29 +0530

    Update next.config.js

--- pdfchat/next.config.js
+++ pdfchat/next.config.js
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-// const nextRuntimeEnv = require('next-runtime-env');
-
-// module.exports = configureRuntimeEnv({
-//   publicPrefix: 'NEXT_PUBLIC_',
-// });
-
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
 module.exports = {
   webpack(config) {
     config.resolve.fallback = {
       fs: false,
     };
     return config;
-  },
+  }
 };
